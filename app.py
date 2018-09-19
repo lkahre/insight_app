99d018f5dcd1653d0af284fe4485fcee1a53d640
lkahre <lekahre@gmail.com>
2018-09-19 17:32:40 -0600

    add print letters

--- app.py
+++ app.py
@@ -1,20 +1,25 @@
 from flask import Flask, render_template, request
+from get_letters import get_letters
 app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 
 def index():
     errors = []
+    letters = []
     trends = ''
     if request.method == "POST":
         #get url that the user has entered
         try:
             word = request.form['word']
+            letters = get_letters(word)
             #print statements just print to terminal
             print("word was:")
             print(word)
         except:
+            errors.append("Unable to get URL. Please make sure it's valid and try again."
+                         )
             print("error")
-    return render_template('index.html')
+    return render_template('index.html', letters=letters, errors=errors)
     
 def hello():
     return "Insight web app created by Lauren Kahre."
