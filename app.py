b64df1c6342f0aa21ecbe1a062aff0fcb5779c9d
lkahre <lekahre@gmail.com>
2018-09-21 15:11:10 -0600

    add redirect to results page

--- app.py
+++ app.py
@@ -8,7 +8,11 @@ import base64
 
 app = Flask(__name__, static_url_path='/static')
 @app.route('/', methods=['GET', 'POST']) 
-def index():
+def index():           
+    return render_template('index.html')
+
+@app.route('/recommendation', methods=['GET', 'POST']) 
+def recommendation():
     errors = []
     letters = []
     admit_class = []
@@ -32,27 +36,11 @@ def index():
             sector = float(sector[:2])
             probs, top3probs = calc_probabilities(admit_class, edu_level, sector)
             htmltables = [probs.to_html(classes="probs"), top3probs.to_html(classes="top3probs")]
-            titles = ['Probabilities by Month', 'Top Three Months']
-            
-            #img = io.BytesIO()
-            #fig = plt.figure(figsize=(9.8,5), dpi=100)
-            #xaxis_loc = np.arange(12)
-            #fig.patch.set_facecolor('w')
-            #plt.plot(xaxis_loc, probs['Probability'])
-            #plt.ylim([0, 100])
-            #plt.xticks(xaxis_loc, probs['Month'])
-            #plt.xlabel('Month')
-            #plt.ylabel('% Chance of Acceptance')
-            #plt.title('Visa Chance of Acceptance by Month')
-            #plt.savefig(img, format='png')
-            #img.seek(0)        
-            #plot_url = base64.b64encode(img.getvalue()).decode
         except:
             errors.append("Unable to get URL. Please make sure it's valid and try again."
                          )
             print("error")
-    return render_template('index.html', errors=errors, admit_class = admit_class, sector = sector, edu_level = edu_level, tables=htmltables, titles = titles, plot_url = plot_url)
-    
+    return render_template('recommendation.html', errors=errors, admit_class = admit_class, sector = sector, edu_level = edu_level, tables=htmltables, plot_url = plot_url)
 def hello():
     return "Insight web app created by Lauren Kahre."
 
